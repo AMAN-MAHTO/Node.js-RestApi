03c7fe48d3b1169cd6fdb5810c730240ea9cc089
AMAN-MAHTO <amahto848@gmail.com>
2024-03-03 13:56:25 +0530

    at root path, get documentation

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const contactRoutes = require("./routes/contactRoutes");
 const userRoutes = require("./routes/UserRoutes");
 const errorHandler = require("./middleware/errorHandler");
 const connectDb = require('./config/dbConnection');
+const fs = require("fs");
 
 // connecting to mongo db
 connectDb();
@@ -12,6 +13,18 @@ const port = process.env.PORT || 5000;
 
 // app midleware
 app.use(express.json()); // data parser
+app.get("/",(req, res) => {
+    // Read the content of the documentation file
+    fs.readFile("readme.md", 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading documentation file:', err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+      // Send the documentation content as response
+      res.send(data);
+    });
+  })
 app.use("/api/contacts",contactRoutes);
 app.use("/api/user",userRoutes);
 app.use(errorHandler);
